aeb444c28981d281063ef230aede1ba559e0c1aa
liranbarokas <liran.barokas@applitools.com>
2018-05-08 12:54:36 +0300

    Proxy Server - Bug fixes changing Version number

--- src/main/java/ApplitoolsTestResultHandler/ApplitoolsTestResultsHandler.java
+++ src/main/java/ApplitoolsTestResultHandler/ApplitoolsTestResultsHandler.java
@@ -47,7 +47,7 @@ import com.sun.glass.ui.Size;
 
 public class ApplitoolsTestResultsHandler {
 
-    private static final String VERSION = "1.3.0";
+    private static final String VERSION = "1.3.1";
     protected static final String STEP_RESULT_API_FORMAT = "/api/sessions/batches/%s/%s/?ApiKey=%s&format=json";
     private static final String RESULT_REGEX = "(?<serverURL>^.+)\\/app\\/batches\\/(?<batchId>\\d+)\\/(?<sessionId>\\d+).*$";
     private static final String IMAGE_TMPL = "%s/step %s %s-%s.png";
